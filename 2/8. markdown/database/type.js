5588a0f9aaebc9e9d07f6c8ed56c11d29f8bc4f0
DongHyuk Kim <donghyuk80.kim@gmail.com>
2021-01-31 12:18:01 +0900

    Update Folder name

--- 2/8. markdown/database/type.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  about_me: 1,
-  resume: 2,
-  applications: 3,
-  notification: 4,
-  blog: 5,
-  accounts: 6,
-}
--- 2/8. markdown/triplek/src/router/router.js
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from '/@router/routes.js'
-
-export const router = createRouter({
-  history: createWebHistory(),
-  linkActiveClass: 'active',
-  routes,
-})
